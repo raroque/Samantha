4c67f7952a0645a339857b7dbe5e27916b9a9f1e
Christian Raroque <Chris@christiansmbp2.attlocal.net>
2016-12-28 01:36:17 -0600

    Added some console log stuff

--- src/app.js
+++ src/app.js
@@ -121,6 +121,7 @@ function chunkString(s, len) {
 }
 
 function sendFBMessage(sender, messageData, callback) {
+	console.log('hello world')
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token: FB_PAGE_ACCESS_TOKEN},
