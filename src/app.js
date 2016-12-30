d49cf5109eeb9aa2b5a9945879ce9843eb88db94
Christian Raroque <Chris@christiansmbp2.attlocal.net>
2016-12-29 21:14:07 -0600

    changed some stuff

--- src/app.js
+++ src/app.js
@@ -17,6 +17,12 @@ const FB_PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN;
 const apiAiService = apiai(APIAI_ACCESS_TOKEN, {language: APIAI_LANG, requestSource: "fb"});
 const sessionIds = new Map();
 
+var express = require("express");
+var mysql = require('mysql');
+var app = express();
+app.use(express.logger());
+
+
 function processEvent(event) {
     var sender = event.sender.id.toString();
 
@@ -28,7 +34,6 @@ function processEvent(event) {
             sessionIds.set(sender, uuid.v1());
         }
 
-		seedMongo();
         console.log("Text", text);
 
         let apiaiRequest = apiAiService.textRequest(text,
@@ -85,96 +90,7 @@ function processEvent(event) {
     }
 }
 
-function seedMongo() {
-	var mongodb = require('mongodb');
 
-// Create seed data
-
-var seedData = [
-  {
-    decade: '1970s',
-    artist: 'Debby Boone',
-    song: 'You Light Up My Life',
-    weeksAtOne: 10
-  },
-  {
-    decade: '1980s',
-    artist: 'Olivia Newton-John',
-    song: 'Physical',
-    weeksAtOne: 10
-  },
-  {
-    decade: '1990s',
-    artist: 'Mariah Carey',
-    song: 'One Sweet Day',
-    weeksAtOne: 16
-  }
-];
-
-// Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
-
-var uri = 'mongodb://heroku_pjktj26b:4g8si3lacn97htocjh02snl90s@ds145158.mlab.com:45158/heroku_pjktj26b';
-
-mongodb.MongoClient.connect(uri, function(err, db) {
-  
-  if(err) throw err;
-  
-  /*
-   * First we'll add a few songs. Nothing is required to create the 
-   * songs collection; it is created automatically when we insert.
-   */
-
-  var songs = db.collection('songs');
-
-   // Note that the insert method can take either an array or a dict.
-
-  songs.insert(seedData, function(err, result) {
-    
-    if(err) throw err;
-
-    /*
-     * Then we need to give Boyz II Men credit for their contribution
-     * to the hit "One Sweet Day".
-     */
-
-    songs.update(
-      { song: 'One Sweet Day' }, 
-      { $set: { artist: 'Mariah Carey ft. Boyz II Men' } },
-      function (err, result) {
-        
-        if(err) throw err;
-
-        /*
-         * Finally we run a query which returns all the hits that spend 10 or
-         * more weeks at number 1.
-         */
-
-        songs.find({ weeksAtOne : { $gte: 10 } }).sort({ decade: 1 }).toArray(function (err, docs) {
-
-          if(err) throw err;
-
-          docs.forEach(function (doc) {
-            console.log(
-              'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] + 
-              ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.'
-            );
-          });
-         
-          // Since this is an example, we'll clean up after ourselves.
-          songs.drop(function (err) {
-            if(err) throw err;
-           
-            // Only close the connection when your app is terminating.
-            db.close(function (err) {
-              if(err) throw err;
-            });
-          });
-        });
-      }
-    );
-  });
-});
-}
 
 function splitResponse(str) {
     if (str.length <= 320) {
